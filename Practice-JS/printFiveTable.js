a8db9e727c960db8fcdf4e5fc1d639d89be1e329
VasaR <vasanthias52@gmail.com>
2024-03-14 01:31:14 +0530

    just a practice

---
+++ Practice-JS/printFiveTable.js
@@ -0,0 +1,6 @@
+let ans = 0;
+
+for(let i = 1; i <= 10; i++){
+    ans = i * 5;
+    console.log(i, "* 5 =",ans );
+}
---
+++ Practice-JS/validTriangle.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 3;
+let c = 6;
+
+if(a+b>c && a+c>b && b+c>a){
+    console.log("valid triangle");
+}
+else{
+    console.log("Not a valid one");
+}
